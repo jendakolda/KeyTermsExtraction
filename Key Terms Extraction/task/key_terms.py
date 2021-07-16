9a182959513fd0dc853c43a2a1865abcb4a803f2
Já <email@jendakolda.cz>
2021-07-16 21:03:04 +0200

    Stage 2 finished

--- Key Terms Extraction/task/key_terms.py
+++ Key Terms Extraction/task/key_terms.py
@@ -1,8 +1,10 @@
 # Write your code here
-# import lxml
+import string
+from collections import Counter
 
 from bs4 import BeautifulSoup
-from nltk import FreqDist
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
 
@@ -14,19 +16,36 @@ class KeyTermsExtractor:
             self.file = file
         else:
             self.file = input('Specify file to evaluate and its path:\n')
+        self.sw = stopwords.words('english')
+        self.punctuation = list(string.punctuation)
+
+    def clean_list(self, words: list):
+        return [word for word in words if (word not in self.sw) and (word not in self.punctuation)]
+
+    def tokenizer(self, text, n_most_freq=5):
+        word_list = word_tokenize(text.lower())
+        word_list = self.lemmatizer(word_list)
+        word_list = self.clean_list(word_list)
+        counted_dict = Counter(sorted(word_list, reverse=True))
+        return [x[0] for x in counted_dict.most_common(n_most_freq)]
+
+    def lemmatizer(self, words):
+        wnl = WordNetLemmatizer()
+        lemmatized = list()
+        for word in words:
+            lemmatized.append(wnl.lemmatize(word))
+        return lemmatized
 
     def parser(self):
         xmlfile = open(self.file, "r").read()
         soup = BeautifulSoup(xmlfile, 'xml')
         all_values = soup.find_all('value')
-        for item in all_values:
-            if item.get('name') == 'head':
-                print(item.get_text() + ':')
-            elif item.get('name') == 'text':
-                five_word_tokens = FreqDist(word_tokenize(item.get_text().lower())).most_common(5)
-                five_words = [five_word_tokens[i][0] for i, v in enumerate(five_word_tokens)]
-                print(*five_words, '\n')
-                # print(*word_tokens[:5], '\n')
+        for tag in all_values:
+            if tag.get('name') == 'head':
+                print(tag.get_text() + ':')
+            elif tag.get('name') == 'text':
+                five_most_common = self.tokenizer(tag.get_text())
+                print(*five_most_common, '\n')
 
 
 if __name__ == '__main__':
