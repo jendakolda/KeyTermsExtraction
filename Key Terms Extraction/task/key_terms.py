4e91b89c67d09d9d233ae0bcd3b4b9c2f3a3de9e
jkolda <jkolda@casaleproject.cz>
2021-07-16 18:03:51 +0200

    Stage 2 unfinished

--- Key Terms Extraction/task/key_terms.py
+++ Key Terms Extraction/task/key_terms.py
@@ -1,9 +1,11 @@
 # Write your code here
-# import lxml
+import string
 
 from bs4 import BeautifulSoup
 from collections import Counter
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
 
 class KeyTermsExtractor:
@@ -14,6 +16,25 @@ class KeyTermsExtractor:
             self.file = file
         else:
             self.file = input('Specify file to evaluate and its path:\n')
+        self.sw = stopwords.words('english')
+        self.punctuation = list(string.punctuation)
+
+    def clean_list(self, words: list):
+        return [word for word in words if (word not in self.sw) and (word not in self.punctuation)]
+
+    def tokenizer(self, text, n_most_freq=5):
+        word_list = word_tokenize(text.lower())
+        word_list = self.clean_list(word_list)
+        word_list = self.lemmatizer(word_list)
+        counted_dict = Counter(sorted(word_list, reverse=True))
+        return [x[0] for x in counted_dict.most_common(n_most_freq)]
+
+    def lemmatizer(self, words):
+        wnl = WordNetLemmatizer()
+        lemmatized = list()
+        for word in words:
+            lemmatized.append(wnl.lemmatize(word))
+        return lemmatized
 
     def parser(self):
         xmlfile = open(self.file, "r").read()
@@ -23,10 +44,8 @@ class KeyTermsExtractor:
             if tag.get('name') == 'head':
                 print(tag.get_text() + ':')
             elif tag.get('name') == 'text':
-                word_list = word_tokenize(tag.get_text().lower())
-                counted_dict = Counter(sorted(word_list, reverse=True))
-                print(*[x[0] for x in counted_dict.most_common(5)])
-
+                five_most_common = self.tokenizer(tag.get_text())
+                print(*five_most_common, '\n')
 
 
 if __name__ == '__main__':
