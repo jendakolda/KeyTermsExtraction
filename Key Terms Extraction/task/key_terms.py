9c7837ac14e9152f2aecc10a1675f8d4992f76c8
Já <email@jendakolda.cz>
2021-07-18 22:39:07 +0200

    Stage 4 unfinished

--- Key Terms Extraction/task/key_terms.py
+++ Key Terms Extraction/task/key_terms.py
@@ -7,53 +7,76 @@ from nltk import pos_tag
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 class KeyTermsExtractor:
     """Program to extract keywords from a XML document"""
+    sw = stopwords.words('english')
+    punctuation = list(string.punctuation)
+    headers = list()
+    vocabulary = list()
 
     def __init__(self, file=None):
         if file:
             self.file = file
         else:
             self.file = input('Specify file to evaluate and its path:\n')
-        self.sw = stopwords.words('english')
-        self.punctuation = list(string.punctuation)
 
-    def clean_list(self, words: list):
-        return [word for word in words if (word not in self.sw) and (word not in self.punctuation)]
-
-    def pos_sort(self, words):
-        return [word for word in words if pos_tag([word])[0][1] == 'NN']
-        # tags = dict(pos_tag(words))
-        # return [k for k, v in tags.items() if v == 'NN']
-
-    def tokenizer(self, text, n_most_freq=5):
-        word_list = word_tokenize(text.lower())
-        word_list = self.lemmatizer(word_list)
-        word_list = self.clean_list(word_list)
-        word_list = self.pos_sort(word_list)
-        counted_dict = Counter(sorted(word_list, reverse=True))
-        return [x[0] for x in counted_dict.most_common(n_most_freq)]
-
-    def lemmatizer(self, words):
+    @staticmethod
+    def lemmatizer(words):
         wnl = WordNetLemmatizer()
         lemmatized = list()
         for word in words:
             lemmatized.append(wnl.lemmatize(word))
         return lemmatized
 
-    def parser(self):
+    @staticmethod
+    def clean_list(words: list):
+        return [word for word in words if (word not in KeyTermsExtractor.sw)
+                and (word not in KeyTermsExtractor.punctuation)]
+
+    @staticmethod
+    def pos_sort(words, part_of_speech='NN'):
+        return [word for word in words if pos_tag([word])[0][1] == part_of_speech]
+
+    @staticmethod
+    def create_tfidf_matrix(dataset):
+        vectorizer = TfidfVectorizer(input='file', use_idf=True, lowercase=True,
+                                     analyzer='word', ngram_range=(1, 1),
+                                     stop_words=None)
+        return vectorizer.fit_transform([dataset])
+
+    def text_processor(self, text):
+        word_list = word_tokenize(text.lower())  # Lists individual words
+        word_list = self.lemmatizer(word_list)  # converts to a dictionary form of lemma
+        word_list = self.clean_list(word_list)  # remove stop words and punctuation
+        word_list = self.pos_sort(word_list)  # filters out only selected pos
+        KeyTermsExtractor.vocabulary.append(' '.join(word_list) + ', ')
+
+    @staticmethod
+    def pick_best_scoring(words, n_most_frequent=5):
+        counted_dict = Counter(sorted(words, reverse=True))
+        return [x[0] for x in counted_dict.most_common(n_most_frequent)]
+
+    def xml_parser(self):
         xmlfile = open(self.file, "r").read()
         soup = BeautifulSoup(xmlfile, 'xml')
         all_values = soup.find_all('value')
         for tag in all_values:
             if tag.get('name') == 'head':
-                print(tag.get_text() + ':')
+                KeyTermsExtractor.headers.append(tag.get_text() + ':')
             elif tag.get('name') == 'text':
-                five_most_common = self.tokenizer(tag.get_text())
-                print(*five_most_common, '\n')
+                self.text_processor(tag.get_text())
+                # print(*five_most_common, '\n')
+
+        # if len(KeyTermsExtractor.headers) == len(KeyTermsExtractor.vocabulary):
+        #     quit('non-matching lengths')
+
+        matrix = self.create_tfidf_matrix(KeyTermsExtractor.vocabulary)
+        for i, header in enumerate(KeyTermsExtractor.headers):
+            print(header)
 
 
 if __name__ == '__main__':
-    KeyTermsExtractor('news.xml').parser()
+    KeyTermsExtractor('news.xml').xml_parser()
